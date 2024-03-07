058c7a4d82a05b0328d8e2c6a508f6f7e7ad2a3b
Scott Hughes <scott_t_hughes@outlook.com>
2024-03-08 08:45:23 +1100

    some css added ...

--- server.js
+++ server.js
@@ -26,11 +26,9 @@ let roomDetails = {}
 
 
 io.on('connection', (socket) => {
-  console.log(`user connected: ${socket.id}`)
 
   socket.on('setup_options', (setupOptions) => {
     socket.join(setupOptions.key)
-    console.log(`joined ${setupOptions.key}`)
     roomDetails[setupOptions.key] = {
       name: setupOptions.name,
       time: setupOptions.time,
@@ -38,28 +36,26 @@ io.on('connection', (socket) => {
       currentround: 1,
       users: [socket.id]
     }
-    console.log(roomDetails)
     io.to(setupOptions.key).emit('recieve_time', setupOptions.time)
   })
   
 
   socket.on('join_room', (roomKey) => {
-    socket.join(roomKey)
-
-    roomDetails[roomKey].users.push(socket.id)
-
-    socket.emit('recieve_time', roomDetails[roomKey].time)
-    socket.emit('set_session_name', roomDetails[roomKey].name)
-    
-    if (roomDetails[roomKey].rounds > 1) {
-      socket.emit('set_round', roomDetails[roomKey].currentround)
+    if(roomDetails[roomKey] !== undefined){
+      socket.join(roomKey)
+  
+      roomDetails[roomKey].users.push(socket.id)
+  
+      socket.emit('recieve_time', roomDetails[roomKey].time)
+      socket.emit('set_session_name', roomDetails[roomKey].name)
+      
+      if (roomDetails[roomKey].rounds > 1) {
+        socket.emit('set_round', roomDetails[roomKey].currentround)
+      }
     }
-
-    console.log(`joined ${roomKey}`)
   })
 
   socket.on('start_timer', (roomKey) => {
-    console.log(roomDetails[roomKey])
     io.to(roomKey).emit('is_round_in_progress', true)
     let timer = startTimer(roomDetails[roomKey].time, roomKey)
     socket.once('stop_timer', () => {
@@ -92,10 +88,6 @@ io.on('connection', (socket) => {
     io.in(roomKey).socketsLeave(roomKey)
     delete roomDetails[roomKey]
   })
-
-
-  console.log(io.engine.clientsCount)
-  console.log(io.of("/").sockets.size)
   
 })
 
@@ -108,18 +100,11 @@ function startTimer(milliseconds, roomKey){
   let time = milliseconds
     const timer = setInterval(() => {
       time = time - 1000
-      console.log(time)
       io.to(roomKey).emit('recieve_time', time)
       if (time === 0){
         clearInterval(timer)
       }
     },1000)
-    
-    // setTimeout(() => {
-    //   clearInterval(timer)
-    //   time = 0
-    //   io.to(roomKey).emit('recieve_time', time)
-    // },time)
   
   return timer
 }
